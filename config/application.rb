bc386edfec37df052c53c4f03cddf939154e4fcf
Clay Shentrup & Luke Wilson <pair+clay+luke@dabohealth.com>
2014-10-28 15:29:45 -0700

    more basic initial setup:
    specify some generator config

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module Comparitron
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.helper false
+    end
   end
 end
