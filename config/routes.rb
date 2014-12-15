397d0501a669655b6bedf9dd7692239de1667a15
clay shentrup <daboeng@lister.local>
2014-12-15 14:54:43 -0800

    Set up Rubocop #83893112
    Fix Rubocop violations in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest
+  # priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -11,7 +12,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions
+  # automatically):
   #   resources :products
 
   # Example resource route with options:
@@ -55,6 +57,6 @@ Rails.application.routes.draw do
   #   end
 
   namespace :api do
-    resources :hospitals, :only => :index
+    resources :hospitals, only: :index
   end
 end
