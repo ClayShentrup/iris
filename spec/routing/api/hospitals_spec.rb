4c2185a03ab2473139810ace09e66bcbf5d0f09d
clay shentrup & Elli Balcomb <pair+brokenladder+ebalcomb@dabohealth.com>
2014-12-15 15:36:03 -0800

    Set up Rubocop #83893112
    Fix Rubocop violations in hospitals routing spec

--- spec/routing/api/hospitals_spec.rb
+++ spec/routing/api/hospitals_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'api/:hospitals' do
-  it "routes to /api/:hospitals/" do
-    expect(:get => "/api/hospitals/?q=xyz").
-      to route_to('api/hospitals#index', :q => "xyz")
+  it 'routes to /api/:hospitals/' do
+    expect(get: '/api/hospitals/?q=xyz')
+      .to route_to('api/hospitals#index', q: 'xyz')
   end
 end
