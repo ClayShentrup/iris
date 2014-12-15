525947cb348ca3074ca899437ace0623c4b0d1c6
clay shentrup <daboeng@lister.local>
2014-12-15 14:54:43 -0800

    Set up Rubocop #83893112
    Fix Rubocop violations in Hospital model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,9 +1,10 @@
+# Represents a hospital entity fetched from Socrata's API.
 class Hospital < ActiveRecord::Base
   validates_uniqueness_of :provider_id
 
   def self.create_or_update(attributes)
     find_or_initialize_by(provider_id: attributes.fetch(:provider_id))
-    .update_attributes!(attributes)
+      .update_attributes!(attributes)
   end
 
   def self.search(query)
