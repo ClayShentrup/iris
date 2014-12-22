cdaccec26eb260b612e7f9f37d99748ed9143702
Jona Haga-Camacho <jona@Jonas-MacBook-Pro.local>
2014-12-22 15:38:34 -0800

    Set up font icons #85027598
    - Add custom font to asset pipeline
    - Import icon-font and icons stylesheets in application.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,5 @@
-Rails.application.config.assets.precompile += ['turbolinks_debugging.js']
+Rails.application.config.tap do |config|
+  config.assets.precompile << 'turbolinks_debugging.js'
+  #config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+  #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+end
