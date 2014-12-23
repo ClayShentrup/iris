44b3a9746ca868f28b3fc4f6f1a8dcb76845da08
Jona Haga-Camacho <jona@Jonas-MacBook-Pro.local>
2014-12-23 08:34:34 -0800

    Set up font icons #85027598
    - Convert .scss files to .sass
    - Remove config.assets.paths from config/application.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,3 @@
 Rails.application.config.tap do |config|
   config.assets.precompile << 'turbolinks_debugging.js'
-  #config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-  #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 end
