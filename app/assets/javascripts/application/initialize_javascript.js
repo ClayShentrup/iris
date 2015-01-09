6be751e7877542a20876dc5a1515c7df26c33de7
Nolan Ehrstrom <daboeng@nightingale.local>
2015-01-08 17:04:44 -0800

    Add JSHint #85321566
    - Exclude _spec.js files
    - Tweak rules in .jshintrc
    - Update js files to be compliant

--- app/assets/javascripts/application/initialize_javascript.js
+++ app/assets/javascripts/application/initialize_javascript.js
@@ -1,12 +1,14 @@
+'use strict';
+
 Dabo = {
   Views: {}
 };
 
 $(document).on('ready page:load', function(){
   var viewName = $('body').data('viewName');
-  var view = Dabo.Views[viewName];
-  if (view) {
-    new view({
+  var View = Dabo.Views[viewName];
+  if (View) {
+    new View({
       el: 'body'
     });
   }
