515daa75b70bb327c212fbd74d1c3aa5e93e8c8f
Ryan Wood & Sean Knox <pair+ryanswood+seanknox@dabohealth.com>
2015-01-09 16:32:57 -0800

    Setup Airbrake to fetch API key from ENV #84438658

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,8 +1,5 @@
-# Revisit checking in Airbrake's Api key. It may not be the best idea to
-# save it here.
-
 if Rails.env.production?
   Airbrake.configure do |config|
-    config.api_key = '809f1ae85c2e20f5480863f674243cd1'
+    config.api_key = ENV.fetch('AIRBRAKE_API_KEY', nil)
   end
 end
