f5bb58a5a2a98fe08dbab296603e8f17b7d371e2
Emilio Cristalli & Sebastian Beceiro <pair+EmilioCristalli+sebabeceiro@dabohealth.com>
2015-01-14 16:34:54 -0200

    Measure Detail View #85731034
    Avoid testing for content in the page

--- spec/controllers/measures_controller_spec.rb
+++ spec/controllers/measures_controller_spec.rb
@@ -9,14 +9,5 @@ RSpec.describe MeasuresController do
     it 'renders the show template' do
       expect(response).to render_template('show')
     end
-
-    it 'renders measure title' do
-      expect(response.body).to include('Communication with Nurses')
-    end
-
-    it 'renders measure description' do
-      expect(response.body).to include('Percent of patients who reported that '\
-        'their nurses "Always" communicated well')
-    end
   end
 end
