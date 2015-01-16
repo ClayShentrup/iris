2404b1ef9eed5acd22fb15ce3bbca92ae8c776a6
clay shentrup & Sean Knox <pair+brokenladder+seanknox@dabohealth.com>
2015-01-15 17:01:34 -0800

    #85731830 First batch of human-friendly VBP routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,35 +8,60 @@ Rails.application.routes.draw do
     resources :pristine_examples
 
     # e.g. /measure_sources/cms/bundles
-    resources :measure_sources, only: [] do
+    resources :measure_sources, only: nil do
       resources :bundles, only: :index
     end
 
-    resources :bundles, only: [] do
+    resources :bundles, only: nil do
       # e.g. /bundles/value-based-purchasing/domains
       resources :domains, only: :index
 
       # e.g.
       # /bundles/readmission-reduction-program/bundle_measures
-      # /bundles/readmission-reduction-program/bundle_measures/30-day-copd
+      # /bundle_measures/30-day-copd
       resources :bundle_measures, only: [:index, :show]
     end
 
-    resources :domains, only: [] do
+    resources :domains, only: nil do
       # e.g.
       # /domains/hospital-acquired-infection/domain_measures
-      # /domains/patient-safety-indicator/domain_measures/patient-safety-composite
+      # /domain_measures/patient-safety-composite
       resources :domain_measures, only: [:index, :show]
 
       # e.g. /domains/outcome-of-care/categories
       resources :categories, only: :index
     end
 
-    resources :categories, only: [] do
+    resources :categories, only: nil do
       # e.g.
       # /categories/mortality/category_measures
-      # /categories/mortality/category_measures/30-day-mortality-ami
+      # /category_measures/30-day-mortality-ami
       resources :category_measures, only: [:index, :show]
     end
+  end
+
+  # Product-friendly route aliases
+
+  scope 'metrics/cms' do
+    # /metrics/cms/value-based-purchasing/outcome-of-care/30-day-mortality-ami
+    get '/value-based-purchasing/outcome-of-care/30-day-mortality-ami',
+        to: 'category_measures#show',
+        defaults: {
+          category_measure_id: '30-day-mortality-ami',
+        }
+
+    # /metrics/cms/value-based-purchasing/outcome-of-care/30-day-mortality-heart-failure
+    get '/value-based-purchasing/outcome-of-care/30-day-mortality-heart-failure',
+        to: 'category_measures#show',
+        defaults: {
+          category_measure_id: '30-day-mortality-heart-failure',
+        }
+
+    # /metrics/cms/value-based-purchasing/outcome-of-care/30-day-mortality-pneumonia
+    get '/value-based-purchasing/outcome-of-care/30-day-mortality-pneumonia',
+        to: 'category_measures#show',
+        defaults: {
+          category_measure_id: '30-day-mortality-pneumonia',
+        }
   end
 end
