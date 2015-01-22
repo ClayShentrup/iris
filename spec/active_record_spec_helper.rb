1c66a1a064963dbe24d1be6ef66db43a34a4876d
Nolan Ehrstrom & Sean Knox <pair+nolanpro+seanknox@dabohealth.com>
2015-01-22 14:45:49 -0800

    [85755328] Add admin user to seeds, tests setup fixes

--- spec/active_record_spec_helper.rb
+++ spec/active_record_spec_helper.rb
@@ -7,7 +7,7 @@ unless Rails.application.present?
 
   # Runs rake db:test:prepare before test run
   if ActiveRecord::Migrator.needs_migration?
-    require_relative '../config/application'
+    require 'rails_helper'
     ActiveRecord::Base.maintain_test_schema = true
     ActiveRecord::Migration.maintain_test_schema!
   end
