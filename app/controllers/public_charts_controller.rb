e4850a8597dbd84b7320a7ca29e5cddafa327d39
Jona Haga-Camacho <jona@dabohealth.com>
2015-01-22 23:36:10 -0800

    [86669642] Complete Navigation on All Screens
    - Add new class for rows without padding
    - DRYed up responsive_grid media queries
    - Add h2 font size
    - Add class to hide teaser charts on mobile if node
      is not a parent of a measure detail

--- app/controllers/public_charts_controller.rb
+++ app/controllers/public_charts_controller.rb
@@ -2,7 +2,23 @@
 # CMS. They are not customizable for a client, thus they are not backed by
 # dynamic data, e.g. in the database.
 class PublicChartsController < ApplicationController
+  helper_method :metric_detail_page?, :category_page?
+
   def show
     @node = PUBLIC_CHARTS_TREE.find(params.fetch(:id))
+  end
+
+  def metric_detail_page?
+    children.empty?
+  end
+
+  def category_page?
+    children.first.children.empty?
+  end
+
+  private
+
+  def children
+    @node.children
   end
 end
