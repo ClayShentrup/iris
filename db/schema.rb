ed7e2c2efc2c96b54fddb45e962964c24fd25e4d
richa & Sean Knox <pair+richaagarwal+seanknox@dabohealth.com>
2015-01-23 16:26:15 -0800

    Re-add pg_stat_statements extension that was removed erroneously

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150123220837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "dimension_samples", force: :cascade do |t|
     t.string   "socrata_provider_id"
