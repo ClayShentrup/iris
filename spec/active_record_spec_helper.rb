0fdbf7c800d1e2266df5c99af89083475a906e8e
Emilio Cristalli & Jorge Bejar <pair+EmilioCristalli+jmbejar@dabohealth.com>
2015-01-26 14:18:18 -0200

    Ensure Test DB schema is always maintained
    - require 'factory_girl' instead of 'factory_girl_rails' to avoid loading every Rails module

--- spec/active_record_spec_helper.rb
+++ spec/active_record_spec_helper.rb
@@ -1,14 +1,9 @@
-require 'rails_common_spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
 
-unless Rails.application.present?
-  FactoryGirl.find_definitions
+unless defined? ActiveRecord
+  require 'active_record'
   connection_info = YAML.load_file('config/database.yml').fetch('test')
   ActiveRecord::Base.establish_connection(connection_info)
-
-  # Runs rake db:test:prepare before test run
-  if ActiveRecord::Migrator.needs_migration?
-    require_relative '../config/application'
-    ActiveRecord::Base.maintain_test_schema = true
-    ActiveRecord::Migration.maintain_test_schema!
-  end
 end
+
+require 'rails_common_spec_helper'
--- spec/rails_common_spec_helper.rb
+++ spec/rails_common_spec_helper.rb
@@ -1,6 +1,12 @@
 require 'active_record'
 require 'shoulda/matchers'
 
+if ActiveRecord::Migrator.needs_migration?
+  require_relative '../config/application'
+  ActiveRecord::Base.maintain_test_schema = true
+  ActiveRecord::Migration.maintain_test_schema!
+end
+
 RSpec.configure do |config|
   config.around do |example|
     ActiveRecord::Base.transaction do
@@ -9,6 +15,7 @@ RSpec.configure do |config|
     end
   end
 
-  require 'factory_girl_rails'
+  require 'factory_girl'
+  FactoryGirl.find_definitions
   config.include(FactoryGirl::Syntax::Methods)
 end
