0decdff4055ba7d31ccc929442a40ace4f2033a8
Jorge Bejar <jorge@wyeworks.com>
2015-01-26 20:33:06 -0200

    [86835722] Use singular resource for News Feed page
    - Using singular resorce in routes file forced to rename controller
    to its plural form: NewsFeedsController (note that we already did it
    for StatusesControllers, following Rails naming conventions)
    - We changed to use the '/news' path to access to this page
    - Basic controller spec added

--- app/controllers/news_feed_controller.rb
+++
@@ -1,5 +0,0 @@
-# Potentially dummy controller, may be needed. For news feed.
-class NewsFeedController < ApplicationController
-  def index
-  end
-end
---
+++ app/controllers/news_feeds_controller.rb
@@ -0,0 +1,6 @@
+# News feed controllers provides a page with the latest activity related with
+# the current user
+class NewsFeedsController < ApplicationController
+  def show
+  end
+end
