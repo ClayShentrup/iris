9974df83ed59c4806b7c5d981c2df8268d9f27fb
Jorge Bejar <jorge@wyeworks.com>
2015-01-27 13:58:29 -0200

    [86835722] Remove logic to replace select item because we will have regular links

--- app/assets/javascripts/application/views/news_feeds/show.js
+++ app/assets/javascripts/application/views/news_feeds/show.js
@@ -5,16 +5,6 @@ Iris.Views['news_feeds-show'] = Backbone.View.extend({
   initialize: function() {
     $('#news_feed_filter').on('click', function() {
       $('.unselected').slideToggle(100);
-      $('.selected_item').addClass('.news_feed_filter_item');
-    });
-    $('.news_feed_filter_item .icon_and_text').click(function() {
-      var clickedItem = $(this);
-      var selectedItem = $('.selected_item .icon_and_text');
-      var clickedItemContent = $(this).html();
-      var selectedItemContent = $('.selected_item .icon_and_text').html();
-
-      $(selectedItem).html(clickedItemContent);
-      $(clickedItem).html(selectedItemContent);
     });
   }
 });
