04e8533dff07a7b30ada383dbb48834320c1646d
Jona Haga-Camacho & Ryan Wood <pair+jonahaga+ryanswood@dabohealth.com>
2015-01-27 12:12:05 -0800

    [86835722] Dropdown arrow changes direction if menu is open

--- app/assets/javascripts/application/views/news_feeds/show.js
+++ app/assets/javascripts/application/views/news_feeds/show.js
@@ -4,10 +4,13 @@
 Iris.Views['news_feeds-show'] = Backbone.View.extend({
   initialize: function() {
     if ($('#news_feed_filter .unselected').is(':hidden')) {
-
       $('#news_feed_filter').on('click', function() {
         $('.unselected').slideToggle(100);
       });
     }
+    $('#news_feed_filter').on('click', function() {
+      $('#news_feed_filter .icon.icon_large.icon_right')
+        .toggleClass('icon_arrow_large_down icon_arrow_large_up');
+    });
   }
 });
