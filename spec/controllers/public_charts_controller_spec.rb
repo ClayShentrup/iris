c14f4548da0b5f3aada97624b443f48071d1abdc
Elli Balcomb & richa <pair+ebalcomb+richaagarwal@dabohealth.com>
2015-01-28 17:09:06 -0800

    Add public charts controller spec and fix node definition in view

---
+++ spec/controllers/public_charts_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe PublicChartsController do
+  describe 'GET show' do
+    login(:user)
+
+    let(:chart_id) do
+      %w[
+        public-data
+        value-based-purchasing
+        outcome-of-care
+        mortality
+        pnuemonia-mortality
+      ].join('/')
+    end
+
+    specify do
+      get :show, id: chart_id
+      expect(response).to be_success
+    end
+  end
+end
