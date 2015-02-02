10111cf85ee3e9a1a751f149e956abfd1aa092e6
Emilio Cristalli <emilio@wyeworks.com>
2015-02-02 18:13:33 -0200

    [87176902] Test select dropdown

---
+++ spec/features/select_and_compare_spec.rb
@@ -0,0 +1,69 @@
+require 'feature_spec_helper'
+
+RSpec.feature 'Select and compare' do
+  login(:user)
+
+  given(:url) do
+    %w[
+      /
+      metrics
+      public-data
+      value-based-purchasing
+      patient-experience-of-care
+      communication
+    ].join('/')
+  end
+
+  def click_on_hospital_select
+    find('.select_container.hospital .dropdown_button').click
+  end
+
+  def click_on_compare_select
+    find('.select_container.compare .dropdown_button').click
+  end
+
+  background do
+    visit url
+  end
+
+  scenario 'open hospital widget' do
+    expect(page).to have_content('Saint Marys Hospital')
+    expect(page).to_not have_content('Hospital Related to User A')
+
+    click_on_hospital_select
+
+    expect(page).to have_content('Hospital Related to User A')
+
+    click_on_hospital_select
+
+    expect(page).to have_content('Saint Marys Hospital')
+    expect(page).to_not have_content('Hospital Related to User A')
+  end
+
+  scenario 'open compare widget' do
+    expect(page).to have_content('Rochester, MN')
+    expect(page).to_not have_content('City, State of Selected Hospital')
+
+    click_on_compare_select
+
+    expect(page).to have_content('Rochester, MN')
+    expect(page).to have_content('City, State of Selected Hospital')
+
+    click_on_compare_select
+
+    expect(page).to have_content('Rochester, MN')
+    expect(page).to_not have_content('City, State of Selected Hospital')
+  end
+
+  scenario 'open hospital widget while compare is open' do
+    click_on_compare_select
+
+    expect(page).to have_content('Rochester, MN')
+    expect(page).to have_content('City, State of Selected Hospital')
+
+    click_on_hospital_select
+
+    expect(page).to have_content('Hospital Related to User A')
+    expect(page).to_not have_content('City, State of Selected Hospital')
+  end
+end
