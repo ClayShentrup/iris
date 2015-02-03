4699a58bb309aae9cbeea5fd36e3d3a136acc5b8
Emilio Cristalli <emilio@wyeworks.com>
2015-02-03 11:50:33 -0200

    [86463294] Rename exclude_blank_password_param method to prevent_blank_password_from_being_saved

--- app/controllers/dabo_admin/users_controller.rb
+++ app/controllers/dabo_admin/users_controller.rb
@@ -9,10 +9,10 @@ module DaboAdmin
       model_params =
         params.require(:user).permit(:email, :is_dabo_admin, :password)
 
-      exclude_blank_password_param(model_params)
+      prevent_blank_password_from_being_saved(model_params)
     end
 
-    def exclude_blank_password_param(params)
+    def prevent_blank_password_from_being_saved(params)
       params.delete(:password) if params.fetch(:password, nil).blank?
       params
     end
