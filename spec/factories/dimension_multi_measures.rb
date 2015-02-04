6377a371cb7bd96578a0559459cc645a8fc377d7
clay shentrup & Ryan Clark <pair+brokenladder+iamryo@dabohealth.com>
2015-02-04 11:21:08 -0800

    [86577944] Fixed a few remaining renaming change remnants for Dimension::MultiMeasure

--- spec/factories/dimension_multi_measures.rb
+++ spec/factories/dimension_multi_measures.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-  factory :dimension_multimeasure, class: 'Dimension::Multimeasure' do
-    provider nil
+  factory :dimension_multi_measures, class: 'Dimension::MultiMeasure' do
+    provider_id '010001'
     measure_id 'MORT_30_AMI'
     column_name 'denominator'
     value '350'
--- spec/models/dimension/multi_measure_spec.rb
+++ spec/models/dimension/multi_measure_spec.rb
@@ -1,15 +1,11 @@
-require 'rails_helper'
+require 'active_record_spec_helper'
 
-RSpec.describe Dimension::Multimeasure do
+RSpec.describe Dimension::MultiMeasure do
   describe 'columns' do
     it { is_expected.to have_db_column(:provider_id).of_type(:string) }
     it { is_expected.to have_db_column(:measure_id).of_type(:string) }
     it { is_expected.to have_db_column(:column_name).of_type(:string) }
     it { is_expected.to have_db_column(:value).of_type(:string) }
-  end
-
-  describe 'indexes' do
-    it { is_expected.to have_db_index(:provider_id) }
   end
 
   describe 'validations' do
