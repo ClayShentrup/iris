885097361b5cf92985a159fa3eaf4c20e8d2783d
Jona Haga-Camacho & richa <pair+jonahaga+richaagarwal@dabohealth.com>
2015-02-09 11:46:27 -0800

    [87177536] getLoad util is only used in one place

--- app/assets/javascripts/application/util/load_hospitals.js
+++ app/assets/javascripts/application/util/load_hospitals.js
@@ -1,13 +1,15 @@
 'use strict';
 
 Iris.Util.loadHospitals = function() {
-  Iris.Util.getLoad(
-    '.default_hospital',
-    '/dabo_admin/accounts/new',
-    {
+  return $.ajax({
+    url: '/dabo_admin/accounts/new',
+    dataType: 'html',
+    data: {
       account: {
         'virtual_system_gid': $('.system_selection option:selected').val()
       }
     }
-  );
+  }).success(function(response) {
+    $('.default_hospital').html(response);
+  });
 };
