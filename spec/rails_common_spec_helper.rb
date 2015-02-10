52aceb3a55c62e9be79e4624dbbcfa72648db85c
Jona Haga-Camacho & richa <pair+jonahaga+richaagarwal@dabohealth.com>
2015-02-10 13:09:57 -0800

    [87177536] Move Database Cleaner setup to support file

--- spec/rails_common_spec_helper.rb
+++ spec/rails_common_spec_helper.rb
@@ -7,29 +7,3 @@ if ActiveRecord::Migrator.needs_migration?
   ActiveRecord::Base.maintain_test_schema = true
   ActiveRecord::Migration.maintain_test_schema!
 end
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, type: :feature) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  require 'factory_girl'
-  FactoryGirl.find_definitions
-  config.include(FactoryGirl::Syntax::Methods)
-end
