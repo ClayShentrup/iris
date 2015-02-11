f4d580d091de1a545de7a82b1905bec2b54b91a4
clay shentrup & Jona Haga-Camacho <pair+brokenladder+jonahaga@dabohealth.com>
2015-02-11 15:22:53 -0800

    Minor simplification of seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,49 +3,43 @@
 # The data can then be loaded with the rake db:seed (or created alongside the
 # db with db:setup).
 
-USERS = [
-  {
-    email: 'admin@dabohealth.com',
-    password: 'timeandcolorisblue',
-    is_dabo_admin: true,
-  },
-  {
-    email: 'plebe@dabohealth.com',
-    password: 'timeandcolorisblue',
-    is_dabo_admin: false,
-  },
-]
+User.create!(
+  [
+    {
+      email: 'admin@dabohealth.com',
+      password: 'timeandcolorisblue',
+      is_dabo_admin: true,
+    },
+    {
+      email: 'plebe@dabohealth.com',
+      password: 'timeandcolorisblue',
+      is_dabo_admin: false,
+    },
+  ],
+  &:skip_confirmation!
+)
 
-HOSPITALS = [
-  {
-    name: 'Dabo Hospital Bel Air',
-    # TODO: is hospital provider id based on hospital or system?
-    provider_id: '999999',
-    city: 'Bel Air',
-    state: 'CA',
-    zip_code: '90077',
-    hospital_type: 'Bourgeois',
-  },
-  {
-    name: 'Dabo Hospital West Philly',
-    provider_id: '999998',
-    city: 'Philadelphia',
-    state: 'PA',
-    zip_code: '19019',
-    hospital_type: 'Playground',
-  },
-]
+HospitalSystem.create!(name: 'Dabo Health System')
 
-HOSPITAL_SYSTEMS = [
-  {
-    name: 'Dabo Health System',
-  },
-]
-User.create!(USERS) do |user|
-  user.skip_confirmation!
-end
-
-HospitalSystem.create!(HOSPITAL_SYSTEMS)
-Hospital.create!(HOSPITALS) do |hospital|
-  hospital.hospital_system = HospitalSystem.find_by_name('Dabo Health System')
+Hospital.create!(
+  [
+    {
+      name: 'Dabo Hospital Bel Air',
+      provider_id: '999999',
+      city: 'Bel Air',
+      state: 'CA',
+      zip_code: '90077',
+      hospital_type: 'Bourgeois',
+    },
+    {
+      name: 'Dabo Hospital West Philly',
+      provider_id: '999998',
+      city: 'Philadelphia',
+      state: 'PA',
+      zip_code: '19019',
+      hospital_type: 'Playground',
+    },
+  ],
+) do |hospital|
+  hospital.hospital_system = HospitalSystem.find_by!(name: 'Dabo Health System')
 end
