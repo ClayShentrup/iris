79eddcc5fcb43733eb99559c15207fa50e48aaec
Jona Haga-Camacho & richa <pair+jonahaga+richaagarwal@dabohealth.com>
2015-02-10 17:01:53 -0800

    [87177536] Alphabetize systems and hospitals in select

--- lib/virtual_hospital_system_collection.rb
+++ lib/virtual_hospital_system_collection.rb
@@ -1,7 +1,17 @@
 # Helper class that returns an array of all hospital systems and hospitals that
 # are not part of systems
 class VirtualHospitalSystemCollection
-  def self.call
+  class << self
+    delegate :call, to: :new
+  end
+
+  def call
+    systems_and_hospitals.sort_by(&:name)
+  end
+
+  private
+
+  def systems_and_hospitals
     HospitalSystem.all + Hospital.without_system
   end
 end
