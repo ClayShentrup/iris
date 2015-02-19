d7f9ca680cf9509d5c23a79a1bbfb020a7bdbef8
Ryan Clark & Ryan Wood <pair+iamryo+ryanswood@dabohealth.com>
2015-02-19 15:05:44 -0800

    [88748724] Fix 'already initialized constant' warning
    - Caused by Sidekiq eager_loading settings.
      See https://github.com/mperham/sidekiq/issues/1791

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,3 +6,7 @@ redis_options = {
 
 Sidekiq.configure_client { |config| config.redis = redis_options }
 Sidekiq.configure_server { |config| config.redis = redis_options }
+
+# Hack to prevent Sidekiq from duplicate loading of explitily required
+# dependencies. See https://github.com/mperham/sidekiq/issues/1791
+ActiveSupport::Dependencies.mechanism = :require
