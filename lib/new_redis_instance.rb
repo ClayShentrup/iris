3486a7f629fa3a8ce14109bbd11d9a6e9e322cc1
Ryan Clark & Ryan Wood <pair+iamryo+ryanswood@dabohealth.com>
2015-02-19 15:38:01 -0800

    [88669966] Change Redis env var config for Sidekiq default behavior
    - Don't parse the env var where the url is, leave it as a value.

--- lib/new_redis_instance.rb
+++ lib/new_redis_instance.rb
@@ -1,6 +1,6 @@
 # Create a new Redis client with the correct config
 module NewRedisInstance
   def self.call
-    Redis.new(url: ENV.fetch('REDIS_PROVIDER', nil))
+    Redis.new(url: ENV.fetch(ENV.fetch('REDIS_PROVIDER')))
   end
 end
