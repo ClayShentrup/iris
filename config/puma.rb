11cd2eec7da70057a344dbf5ac06900295e70b1d
Ryan Clark & Jona Haga-Camacho <pair+iamryo+jonahaga@dabohealth.com>
2015-02-20 12:15:05 -0800

    [86835762] Re-enable connection to nginx socket in development.

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,13 @@
 # the-puma-web-server
 workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
 
+if ENV['RACK_ENV'] == 'development'
+  # bind to socket for nginx - http://iris.dev
+  if ENV.fetch('BOXEN_SOCKET_DIR', nil)
+    bind "unix:#{ENV.fetch('BOXEN_SOCKET_DIR', nil)}/iris"
+  end
+end
+
 # PER HEROKU:
 # Puma allows you to configure your thread pool with a min and max setting,
 # controlling the number of threads each Puma instance uses. The min threads
