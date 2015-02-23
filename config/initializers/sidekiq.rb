d3fb07aba465a112980b5aaba1a37195a14224cc
Emilio Cristalli & Sebastian Beceiro <pair+EmilioCristalli+sebabeceiro@dabohealth.com>
2015-02-23 18:57:59 -0200

    [88942924] Revert "[88748724] Fix 'already initialized constant' warning"
    This reverts commit d7f9ca680cf9509d5c23a79a1bbfb020a7bdbef8.
    
    This was causing Rails auto reload of classes in development to fail

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,7 +6,3 @@ redis_options = {
 
 Sidekiq.configure_client { |config| config.redis = redis_options }
 Sidekiq.configure_server { |config| config.redis = redis_options }
-
-# Hack to prevent Sidekiq from duplicate loading of explitily required
-# dependencies. See https://github.com/mperham/sidekiq/issues/1791
-ActiveSupport::Dependencies.mechanism = :require
