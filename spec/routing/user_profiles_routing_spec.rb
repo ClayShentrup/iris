8c030e92e95b8785b9d1b56fc4af78e6780c83a9
Ryan Clark & Jona Haga-Camacho <pair+iamryo+jonahaga@dabohealth.com>
2015-02-26 13:54:13 -0800

    [88666200] Create user profile info view

--- spec/routing/user_profiles_routing_spec.rb
+++ spec/routing/user_profiles_routing_spec.rb
@@ -4,13 +4,15 @@ RSpec.describe UserProfilesController do
   describe 'user profiles routing' do
     let(:path) { "/user_profiles/#{page_id}" }
 
-    context 'menu' do
-      let(:page_id) { 'menu' }
-      it 'routes to the correct page' do
-        expect(get: path).to route_to(
-          'user_profiles#show',
-          id: page_id,
-          )
+    %w[info menu].each do |page_id|
+      context page_id do
+        let(:page_id) { page_id }
+        it 'routes to the correct page' do
+          expect(get: path).to route_to(
+            'user_profiles#show',
+            id: page_id,
+            )
+        end
       end
     end
   end
