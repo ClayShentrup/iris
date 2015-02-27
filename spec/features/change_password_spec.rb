c6a3fe079507b54a6a76bfa233c0ce4a4dbe3149
Jona Haga-Camacho <jona@dabohealth.com>
2015-02-26 22:11:26 -0800

    [88212336] Use Rails route helper in spec
    - Remove code from method that only gets called
      once and place into spec scenario

--- spec/features/change_password_spec.rb
+++ spec/features/change_password_spec.rb
@@ -2,24 +2,19 @@ require 'feature_spec_helper'
 
 RSpec.feature 'Change password' do
   let(:user) { create :user }
-  let(:user_profiles_settings_url) { '/user_profiles/settings' }
   let(:new_password) { 'composedchosechargeproduce' }
 
   before do
     log_in user
-    visit user_profiles_settings_url
+    visit user_profiles_settings_path
     click_link 'Change password'
   end
 
-  def renew_password
+  scenario 'user can change password from user profiles' do
     fill_in 'Current password', with: user.password
     fill_in 'Password', with: new_password
     fill_in 'Password confirmation', with: new_password
     click_button 'Update'
-  end
-
-  scenario 'user can change password from user profiles' do
-    renew_password
     expect(page).to have_content('Your account has been updated successfully.')
   end
 end
