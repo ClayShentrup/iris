7b5ddb4ec834ddb9ce496a8f2c743e3be6b64ceb
Ryan Clark & Ryan Wood <pair+iamryo+ryanswood@dabohealth.com>
2015-03-04 14:54:27 -0800

    [89295600] Increase rack timeout to 15 seconds

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,5 +1,5 @@
 # See https://devcenter.heroku.com/articles/deploying-rails-applications-with-
 # the-puma-web-server
 unless Rails.env.test?
-  Rack::Timeout.timeout = 5 # seconds
+  Rack::Timeout.timeout = 15 # seconds
 end
