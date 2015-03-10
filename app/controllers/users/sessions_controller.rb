0331f2195f4b0ae62e5963a3e758f9edb1b0ded9
Jona Haga-Camacho & Sean Knox <pair+jonahaga+seanknox@dabohealth.com>
2015-03-10 13:19:26 -0700

    [89480344] Refactor SessionsController

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,6 @@ module Users
   class SessionsController < Devise::SessionsController
     def new
       @reset_password = reset_password?
-      @current_user = current_user unless params.fetch(:user, nil).nil?
       super
     end
 
@@ -18,16 +17,24 @@ module Users
       Sessions::PasswordExpirationPresenter.call(current_user, view_context)
     end
 
-    def current_user
-      @current_user ||= User.find_by_email(params.fetch(:user).fetch(:email))
-    end
-
     def reset_password?
-      if params.keys.include?('user')
+      if user_available?
         current_user.failed_attempts == 2
       else
         false
       end
+    end
+
+    def current_user
+      User.find_by_email(user_email)
+    end
+
+    def user_available?
+      params.keys.include?('user') && current_user
+    end
+
+    def user_email
+      params.fetch(:user).fetch(:email)
     end
   end
 end
