ef58b9a57a8c759302ab9207cc3fd3cb889d39a8
Jona Haga-Camacho & Sean Knox <pair+jonahaga+seanknox@dabohealth.com>
2015-03-10 14:21:14 -0700

    [89480344] User sees reset password message after second failed attempt - Use custom session controller - Add `.disabled` form class and styles - Update flash messages for last attempt and invalid attempt

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,12 @@
 module Users
-  # controller to allow custom login flash message.
+  # Custom user sessions controller
   class SessionsController < Devise::SessionsController
+    def new
+      @reset_password = reset_password?
+      @current_user = current_user unless params.fetch(:user, nil).nil?
+      super
+    end
+
     def create
       super
       flash[:notice] = reminder if reminder
@@ -10,6 +16,18 @@ module Users
 
     def reminder
       Sessions::PasswordExpirationPresenter.call(current_user, view_context)
+    end
+
+    def current_user
+      @current_user ||= User.find_by_email(params.fetch(:user).fetch(:email))
+    end
+
+    def reset_password?
+      if params.keys.include?('user')
+        current_user.failed_attempts == 2
+      else
+        false
+      end
     end
   end
 end
--- spec/features/lock_account_spec.rb
+++ spec/features/lock_account_spec.rb
@@ -3,6 +3,15 @@ require 'feature_spec_helper'
 RSpec.feature 'Lock account' do
   let!(:user) { create(:user_for_controller_specs) }
   let!(:admin_user) { create(:dabo_admin) }
+  let(:first_failed_attempt_message) do
+    'Invalid email or password. ' \
+    'You have one more try before we reset your login.'
+  end
+  let(:second_failed_attempt_message) do
+    'Invalid email or password. ' \
+    'We sent an e-mail with instructions on how to reset your password. ' \
+    "#{user.email}"
+  end
 
   def log_in_with_wrong_password
     visit new_user_session_path
@@ -11,32 +20,34 @@ RSpec.feature 'Lock account' do
     click_button 'Sign in'
   end
 
-  background do
-    log_in_with_wrong_password
-    log_in_with_wrong_password
-  end
+  describe 'when a user incorrectly logs in' do
+    it 'shows the first failed attempt message' do
+      log_in_with_wrong_password
+      expect(page).to have_content(first_failed_attempt_message)
+    end
 
-  scenario 'account is not locked after only 2 failed attempts' do
-    expect(page).to have_content(
-      'You have one more attempt before your account is locked.',
-    )
-    log_in(user)
-    expect(current_path).to eq root_path
-  end
+    it 'shows the second failed attempt message' do
+      log_in_with_wrong_password
+      log_in_with_wrong_password
+      expect(page).to have_content(second_failed_attempt_message)
+    end
 
-  scenario 'account locks after 3 failed attempts, and can be unlocked' do
-    log_in_with_wrong_password
-    log_in(user)
-    expect(page).to have_content 'Your account is locked.'
-    expect(current_path).to eq new_user_session_path
+    it 'locks account after 3 failed attempts, and can be unlocked' do
+      log_in_with_wrong_password
+      log_in_with_wrong_password
+      log_in_with_wrong_password
+      log_in(user)
+      expect(page).to have_content 'Your account is locked.'
+      expect(current_path).to eq new_user_session_path
 
-    log_in(admin_user)
-    visit edit_dabo_admin_user_path(user)
-    click_button 'Unlock'
-    expect(page).not_to have_button 'Unlock'
-    log_out
+      log_in(admin_user)
+      visit edit_dabo_admin_user_path(user)
+      click_button 'Unlock'
+      expect(page).not_to have_button 'Unlock'
+      log_out
 
-    log_in(user)
-    expect(current_path).to eq root_path
+      log_in(user)
+      expect(current_path).to eq root_path
+    end
   end
 end
