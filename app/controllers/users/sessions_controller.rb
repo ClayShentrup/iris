14e8ef973b468e44cfd4bf7acf64cfe5ef502692
Jona Haga-Camacho & Sean Knox <pair+jonahaga+seanknox@dabohealth.com>
2015-03-10 18:59:43 -0700

    [89480344] Move logic from SessionsController to presenter

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,47 +2,8 @@ module Users
   # Custom user sessions controller
   class SessionsController < Devise::SessionsController
     def new
-      @password_errors = {
-        reset_password_message: reset_password_message?,
-        css_class: css_class,
-      }
+      @sessions_presenter = SessionsPresenter.new(params, flash)
       super
-    end
-
-    def create
-      super
-      flash[:notice] = reminder if reminder
-    end
-
-    private
-
-    def reminder
-      Sessions::PasswordExpirationPresenter.call(current_user, view_context)
-    end
-
-    def reset_password_message?
-      if valid_user?
-        current_user.failed_attempts == 2
-      else
-        false
-      end
-    end
-
-    def css_class
-      'field_with_errors' if flash.alert &&
-                             flash.alert.include?('Invalid email or password')
-    end
-
-    def current_user
-      User.find_by_email(user_email)
-    end
-
-    def valid_user?
-      params.keys.include?('user') && current_user
-    end
-
-    def user_email
-      params.fetch(:user).fetch(:email)
     end
   end
 end
---
+++ spec/lib/sessions_presenter_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe SessionsPresenter do
+  subject { described_class.new(params, flash) }
+  let(:flash) { double ActionDispatch::Flash::FlashHash }
+  let(:flash_message) { 'Invalid email or password' }
+
+  def check_reset_password_message(expected_value)
+    expect(subject.reset_password_message?).to be expected_value
+  end
+
+  def check_css(expected_value)
+    expect(subject.css_class).to eq expected_value
+  end
+
+  before do
+    allow(flash).to receive(:alert).and_return(flash_message)
+  end
+
+  context 'when a user visits the sign in page for the first time' do
+    let(:flash_message) { '' }
+    let(:params) do
+      {
+        controller: 'users/sessions',
+        action: 'new',
+      }
+    end
+
+    it 'returns false for reset_password_message? and no CSS class name' do
+      check_reset_password_message(false)
+      check_css(nil)
+    end
+  end
+
+  context 'when a user submits an invalid email address' do
+    let(:params) do
+      HashWithIndifferentAccess.new(
+        controller: 'users/sessions',
+        action: 'create',
+        user: {
+          email: 'donald.draper@scdp.com',
+          password: 'thisismypasswordok!',
+        },
+      )
+    end
+
+    it 'returns false for reset_password_message? and correct CSS class name' do
+      check_reset_password_message(false)
+      check_css('field_with_errors')
+    end
+  end
+
+  context 'when a user submits the incorrect password for second time' do
+    let(:user) { create :user }
+    let(:params) do
+      HashWithIndifferentAccess.new(
+        controller: 'users/sessions',
+        action: 'create',
+        user: {
+          email: user.email,
+          password: 'thisismypasswordok!',
+        },
+      )
+    end
+
+    before do
+      user.update_attribute(:failed_attempts, 2)
+    end
+
+    it 'returns true for reset_password_message? and correct CSS class name' do
+      check_reset_password_message(true)
+      check_css('field_with_errors')
+    end
+  end
+end
