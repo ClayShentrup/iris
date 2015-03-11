2c89b7a5d5653b7eaf4f9caa2cfb038227f05496
Jona Haga-Camacho & Sean Knox <pair+jonahaga+seanknox@dabohealth.com>
2015-03-10 18:07:24 -0700

    [89480344] SessionsController spec

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -4,10 +4,6 @@ RSpec.describe Users::SessionsController do
   let(:user) { build_stubbed :user_with_devise_session }
   simulate_routed_request
 
-  save_fixture do
-    get :new
-  end
-
   describe 'POST #create' do
     let(:user) { create :user }
     let(:valid_params) do
@@ -42,6 +38,68 @@ RSpec.describe Users::SessionsController do
 
       it 'does not show a flash message reminder' do
         expect(flash[:notice]).to_not be_present
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context 'with a valid user' do
+      let(:user) { create :user_with_devise_session }
+      let(:valid_params) do
+        {
+          email: user.email,
+          password: user.password,
+        }
+      end
+
+      save_fixture do
+        get :new
+      end
+
+      simulate_routed_request
+      let!(:set_logged_in_state) { sign_in user }
+
+      before do
+        post :create, user: valid_params
+      end
+
+      it 'logs the user in' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with an invalid user email' do
+      let(:invalid_params) do
+        {
+          email: 'foo@bar.com',
+          password: 'somepassword',
+        }
+      end
+
+      before do
+        post :create, user: invalid_params
+      end
+
+      it 'redirects to sign in page' do
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'with an invalid password' do
+      let(:user) { create :user_with_devise_session }
+      let(:invalid_params) do
+        {
+          email: user.email,
+          password: 'wrongpasswordyo!',
+        }
+      end
+
+      before do
+        post :create, user: invalid_params
+      end
+
+      it 'redirects to sign in page' do
+        expect(response).to render_template :new
       end
     end
   end
