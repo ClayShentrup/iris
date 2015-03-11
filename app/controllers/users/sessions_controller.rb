ce5c57c22a21229e79bf8ef46f72e37dfac46fb0
Jona Haga-Camacho & Sean Knox <pair+jonahaga+seanknox@dabohealth.com>
2015-03-10 17:03:41 -0700

    [89480344] Invalid login submissions should have appropriate styling

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,10 @@ module Users
   # Custom user sessions controller
   class SessionsController < Devise::SessionsController
     def new
-      @reset_password = reset_password?
+      @password_errors = {
+        reset_password_message: reset_password_message?,
+        css_class: css_class,
+      }
       super
     end
 
@@ -17,19 +20,24 @@ module Users
       Sessions::PasswordExpirationPresenter.call(current_user, view_context)
     end
 
-    def reset_password?
-      if user_available?
+    def reset_password_message?
+      if valid_user?
         current_user.failed_attempts == 2
       else
         false
       end
     end
 
+    def css_class
+      'field_with_errors' if flash.alert &&
+                             flash.alert.include?('Invalid email or password')
+    end
+
     def current_user
       User.find_by_email(user_email)
     end
 
-    def user_available?
+    def valid_user?
       params.keys.include?('user') && current_user
     end
 
---
+++ spec/features/login_validation_spec.rb
@@ -0,0 +1,76 @@
+require 'feature_spec_helper'
+
+RSpec.feature 'Login validation' do
+  let!(:user) { create(:user) }
+  let!(:admin_user) { create(:dabo_admin) }
+  let(:first_failed_attempt_message) do
+    'Invalid email or password. ' \
+    'You have one more try before we reset your login.'
+  end
+  let(:second_failed_attempt_message) do
+    'Invalid email or password. ' \
+    'We sent an e-mail with instructions on how to reset your password. ' \
+    "#{user.email}"
+  end
+  let(:incorrect_email_password_message) do
+    'Invalid email or password. ' \
+    'Please make sure you are using a hospital email.'
+  end
+
+  let(:email_field) { '.email_field' }
+  let(:password_field) { '.password_field' }
+
+  def log_in_with_wrong_credentials(email, password)
+    visit new_user_session_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Login'
+  end
+
+  def check_input_field_on_error_styling
+    within find('form#new_user') do
+      expect(page).to have_css('.email_field.field_with_errors')
+      expect(page).to have_css('.password_field.field_with_errors')
+    end
+  end
+
+  describe 'when a invalid user attempts to login' do
+    it 'returns an incorrect email or password message' do
+      log_in_with_wrong_credentials('foo@bar.com', 'wrongpassword')
+      expect(page).to have_content(incorrect_email_password_message)
+      check_input_field_on_error_styling
+    end
+  end
+
+  describe 'when a valid user incorrectly logs in' do
+    it 'shows the first failed attempt message' do
+      log_in_with_wrong_credentials(user.email, 'wrongpassword')
+      expect(page).to have_content(first_failed_attempt_message)
+      check_input_field_on_error_styling
+    end
+
+    it 'shows the second failed attempt message' do
+      log_in_with_wrong_credentials(user.email, 'wrongpassword')
+      log_in_with_wrong_credentials(user.email, 'wrongpassword')
+      expect(page).to have_content(second_failed_attempt_message)
+    end
+
+    it 'locks account after 3 failed attempts, and can be unlocked' do
+      log_in_with_wrong_credentials(user.email, 'wrongpassword')
+      log_in_with_wrong_credentials(user.email, 'wrongpassword')
+      log_in_with_wrong_credentials(user.email, 'wrongpassword')
+      log_in(user)
+      expect(page).to have_content 'Your account is locked.'
+      expect(current_path).to eq new_user_session_path
+
+      log_in(admin_user)
+      visit edit_dabo_admin_user_path(user)
+      click_button 'Unlock'
+      expect(page).not_to have_button 'Unlock'
+      log_out
+
+      log_in(user)
+      expect(current_path).to eq root_path
+    end
+  end
+end
