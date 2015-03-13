a22e70b678c3eb466aab60103142595db4233772
Ryan Clark & Jona Haga-Camacho <pair+iamryo+jonahaga@dabohealth.com>
2015-03-12 17:26:18 -0700

    [89480344] Combine two JS files that were for same view

--- app/assets/javascripts/application/views/sessions/new.js
+++ app/assets/javascripts/application/views/sessions/new.js
@@ -1,9 +1,13 @@
-// jshint devel: true
+// jshint nonew: false
 'use strict';
 
 Iris.Views['sessions-new'] = Backbone.View.extend({
   events: {
     'click .cancel_btn': '_clearLoginForm'
+  },
+
+  initialize: function() {
+    new Iris.Views.passwordField({el: '.toggle_password'});
   },
 
   _clearLoginForm: function() {
--- spec/javascripts/views/sessions_view_spec.js
+++ spec/javascripts/views/sessions_view_spec.js
@@ -2,14 +2,17 @@
 'use strict';
 
 describe('SessionsView', function() {
+  beforeEach(function() {
+    loadFixture(
+      'users-sessions_controller'
+    );
+  });
+
   describe('clicking the Cancel button', function() {
     var loginFields;
     var cancelButton;
 
     beforeEach(function() {
-      loadFixture(
-        'users-sessions_controller'
-      );
       new Iris.Views['sessions-new']({el: '#body'});
 
       loginFields = $('input.form_control');
@@ -21,6 +24,55 @@ describe('SessionsView', function() {
       cancelButton.click();
 
       expect(loginFields.val()).toBe('');
+    });
+  });
+
+  describe('toggling password masking', function() {
+    var view;
+    var passwordField;
+    var toggleIcon;
+
+    beforeEach(function() {
+      view = new Iris.Views.passwordField({el: '.toggle_password'});
+      passwordField = $('#user_password');
+      toggleIcon = $('.toggle_password .icon');
+    });
+
+    describe('with text in password input', function() {
+      beforeEach(function() {
+        passwordField.val('password').change();
+      });
+
+      it('shows eye icon', function() {
+        expect(toggleIcon).toBeVisible();
+      });
+
+      it('eye icon toggles password mask', function() {
+        expect(passwordField).toHaveAttr('type', 'password');
+        toggleIcon.click();
+        expect(passwordField).toHaveAttr('type', 'text');
+        toggleIcon.click();
+        expect(passwordField).toHaveAttr('type', 'password');
+      });
+    });
+
+    describe('without text in password input', function() {
+      beforeEach(function() {
+        passwordField.val('').change();
+      });
+
+      it('eye icon is hidden', function() {
+        expect(toggleIcon).toBeHidden();
+      });
+    });
+
+    it('does not allow to paste text', function() {
+      spyOn(view, '_disablePaste');
+      view.delegateEvents();
+
+      passwordField.trigger('paste');
+
+      expect(view._disablePaste).toHaveBeenCalled();
     });
   });
 });
