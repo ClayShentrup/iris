6d9010a9a0ec270b70f825a6ac5a19bd81ac91e7
Sean Knox <knoxville@gmail.com>
2015-03-18 19:20:13 -0700

    [90663462] Add missing test coverage for HospitalSystem model

--- spec/models/hospital_system_spec.rb
+++ spec/models/hospital_system_spec.rb
@@ -11,6 +11,7 @@ require './app/models/hospital_system'
 
 RSpec.describe HospitalSystem do
   it { is_expected.to have_many(:providers) }
+  it { is_expected.to have_one(:account) }
 
   describe 'columns' do
     specify do
@@ -22,6 +23,17 @@ RSpec.describe HospitalSystem do
   end
 
   describe 'validations' do
+    subject { build_stubbed(described_class) }
+
+    it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe '#count' do
+    it do
+      is_expected.to delegate_method(:providers_count)
+        .to(:providers)
+        .as(:count)
+    end
   end
 end
