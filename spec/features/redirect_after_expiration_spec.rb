31e71b7d27842868d2a12759007baad55db0eb10
Emilio Cristalli & richa & Sebastian Beceiro <pair+emiliocristalli+richaagarwal+sebabeceiro@dabohealth.com>
2015-03-24 11:38:51 -0300

    [90069578] Add test to ensure redirect URL after sign in is correct
    - When the session expires the user gets redirected to the page that he was
    navigating to after login.

---
+++ spec/features/redirect_after_expiration_spec.rb
@@ -0,0 +1,21 @@
+require 'feature_spec_helper'
+
+RSpec.feature 'Clicking a link after session expires' do
+  let(:user) { create(User, :with_associations) }
+  let(:url) { '/metrics/public-data/hospital-acquired-conditions' }
+
+  before { log_in user }
+
+  it 'redirects to link target after signing back in' do
+    visit url
+    find('#measures_nav_container').click_link('Patient Safety Indicator')
+    expect(page).to have_selector('h2', text: 'Patient Safety Indicator')
+
+    logout
+
+    click_link 'Hospital-Acquired Conditions'
+    expect(page).to have_text('Sign In')
+    log_in user
+    expect(current_path).to eq url
+  end
+end
