0bf8aeb80992cd9c5db680511103b3dd905750c3
Ryan Clark & Jona Haga-Camacho <pair+iamryo+jonahaga@dabohealth.com>
2015-03-24 10:51:58 -0700

    Revert "Merge pull request #163 from dabohealth/chores/test_devise_version_90930174"
    This reverts commit ceb3a6bce8fae07ebf17cda0c6b0a68f899e61fa, reversing
    changes made to b1d872a6297323a11d6957c12eecad20e9ae8663.

--- spec/devise_version_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'devise/version'
-
-RSpec.describe 'Devise version' do
-  it 'is 3.4.1' do
-    # Reminder to update the Users::RegistrationsController once Devise is
-    # updated and the build_resource method override is no longer necessary
-    expect(Devise::VERSION).to eq '3.4.1'
-  end
-end
