144d95f2843561c33b7343a29f7a2bb627edb532
Ryan Clark & Jona Haga-Camacho <pair+iamryo+jonahaga@dabohealth.com>
2015-03-24 17:24:02 -0700

    [89480536] Forgot Password form is prepopulated with user's email
    ... if the user has attempted a login once before visiting
    Forgot Password.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,15 +2,14 @@ module Users
   # Custom user sessions controller
   class SessionsController < Devise::SessionsController
     def new
-      last_attempt_handler = Sessions::LastAttemptHandler.new(
-        resource_params.fetch(:email, nil),
-      )
+      last_attempt_handler = Sessions::LastAttemptHandler.new(user_email)
       @is_last_attempt = last_attempt_handler.last_attempt?
       super
       last_attempt_handler.send_reset_password_instructions_if_last_attempt
     end
 
     def create
+      session[:session_email] = user_email
       super
       flash[:notice] = reminder if reminder
     end
@@ -19,6 +18,10 @@ module Users
 
     def reminder
       Sessions::PasswordExpirationPresenter.call(current_user, view_context)
+    end
+
+    def user_email
+      resource_params.fetch(:email, nil)
     end
   end
 end
