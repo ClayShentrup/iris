bbb2492c6a615da2a3979e495fbbd77ddd5a7577
Emilio Cristalli & Sebastian Beceiro <pair+emiliocristalli+sebabeceiro@dabohealth.com>
2015-03-25 16:31:54 -0300

    Keep annotations up to date

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,8 +4,8 @@
 #
 #  id              :integer          not null, primary key
 #  content         :text             not null
-#  author_id       :integer          not null
-#  conversation_id :integer          not null
+#  author_id       :integer
+#  conversation_id :integer
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
