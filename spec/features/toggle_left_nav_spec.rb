81afb07e4da3e7b36588621ec8324ebbc82e7c05
Jona Haga-Camacho & Sean Knox <pair+jonahaga+seanknox@dabohealth.com>
2015-03-31 11:43:40 -0700

    [91300234] Ensure tablet_portrait off canvas menu can be toggled

--- spec/features/toggle_left_nav_spec.rb
+++ spec/features/toggle_left_nav_spec.rb
@@ -4,23 +4,29 @@ RSpec.feature 'Toggling left navigation' do
   login_user
 
   background do
-    resize_to(:mobile)
+    resize_to(width)
     visit root_path
   end
 
-  def ensure_sidebar_is_not_visible
+  def check
     expect(page).to have_css('.sidebar_offcanvas', visible: false)
-  end
-
-  def ensure_sidebar_is_visible
+    find('.menu_icon').click
     expect(page).to have_css('.sidebar_offcanvas', visible: true)
   end
 
   feature 'revealing the left navigation on mobile' do
+    let(:width) { :mobile }
+
     scenario do
-      ensure_sidebar_is_not_visible
-      find('.menu_icon').click
-      ensure_sidebar_is_visible
+      check
+    end
+  end
+
+  feature 'reavling the left navigation on tablet portrait' do
+    let(:width) { :tablet_portrait }
+
+    scenario do
+      check
     end
   end
 end
